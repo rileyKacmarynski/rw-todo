19ecc32bed5ba4b6cd65683d241309e751c9eac5
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2022-08-07 07:52:22 -0500

    Got auth setup

---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+import { AuthProvider } from '@redwoodjs/auth'
+
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import './scaffold.css'
+import './index.css'
+
+const App = () => (
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+      <AuthProvider type="dbAuth">
+        <ColorModeScript />
+        <ChakraProvider>
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
+    </RedwoodProvider>
+  </FatalErrorBoundary>
+)
+
+export default App
